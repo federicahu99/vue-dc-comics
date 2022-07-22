4f58ad31262e2297e160dbbb35b559e8c5adc8ce
feddericahu99 <federica.hu339@gmail.com>
2022-07-22 16:40:15 +0200

    header component

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,18 +1,21 @@
 <template>
-  <header>
-    <figure>
-        <img src="../assets/img/dc-logo.png" alt="logo DC">
-    </figure>
-    <nav>
-        <ul>
-            <li v-for="link in links" :key="link.text">
-            <a 
-                :href="link.url"> 
-                {{ link.text }} 
-            </a>
-            </li>
-        </ul>
-    </nav>
+  <header class="container">
+    <div>
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="logo DC">
+        </figure>
+        <nav>
+            <ul>
+                <li v-for="link in links" :key="link.text">
+                <a 
+                    :href="link.url"
+                    :class="{active: link.current}"> 
+                    {{ link.text }} 
+                </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
   </header>
 </template>
 
@@ -22,6 +25,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+header {
+    height: 150px;
+    display: flex;
+    justify-content: space-between;
 
+    figure {
+        height: 50pxs;
+    }
+}
 </style>
