60d61991b577b0d7fdfb9f654e1ecb5b8b26b8d4
feddericahu99 <federica.hu339@gmail.com>
2022-07-22 17:02:28 +0200

    BaseHeader

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="container">
-    <div>
+    <div id="header-flex">
         <figure>
             <img src="../assets/img/dc-logo.png" alt="logo DC">
         </figure>
@@ -21,18 +21,96 @@
 
 <script>
 export default {
-    name: 'BaseHeader'
+    name: 'BaseHeader',
+    data(){ 
+    return {
+      links: [
+    {
+    text: 'Characters',
+    url: '#',
+    current: false,
+    },
+    {
+     text: 'Comics',
+     url: '#',
+     current: true,
+    },
+    {
+     text: 'Movies',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'TV',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'Games',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'Collectibles',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'Videos',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'Fans',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'News',
+     url: '#',
+     current: false,
+   },
+   {
+     text: 'Shop',
+     url: '#',
+     current: false,
+   }
+   ]}
+  }
 }
+
 </script>
 
 <style lang="scss" scoped>
 header {
-    height: 150px;
-    display: flex;
-    justify-content: space-between;
+    height: 80px;
+    
+    #header-flex{
+        height: 80%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
     figure {
-        height: 50pxs;
+        height: 90%;
+
+        img{
+            height: 90%;
+            margin-top: 7px;
+        }
+    }
+
+    ul {
+        display: flex;
+        
+        li{
+            padding: 10px;
+            margin-right: 10px;
+            text-transform: uppercase;
+            font-size: 10px;
+            font-weight: bolder;
+        }
     }
 }
 </style>
