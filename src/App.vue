e5e0f2a9c3713c334f509f81b818581c07a0e92c
feddericahu99 <federica.hu339@gmail.com>
2022-07-22 16:10:32 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,77 @@
 <template>
   <div>
+    <BaseHeader />
     
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseHeader from './components/BaseHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    BaseHeader,
+  },
+  data(){ 
+    return {
+      links: [
+  {
+    text: 'Characters',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Comics',
+    url: '#',
+    current: true,
+  },
+  {
+    text: 'Movies',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'TV',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Games',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Collectibles',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Videos',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Fans',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'News',
+    url: '#',
+    current: false,
+  },
+  {
+    text: 'Shop',
+    url: '#',
+    current: false,
+  }
+  ]}
   }
 }
+
 </script>
 
 <style lang="scss">
-
+@import "./assets/sass/style.scss";
 </style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <figure>
+        <img src="../assets/img/dc-logo.png" alt="">
+    </figure>
+    <nav>
+        <ul>
+            <li v-for="link in links" :key="link.text">
+            <a 
+                :href="link.url"> 
+                {{ link.text }} 
+            </a>
+            </li>
+        </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: BaseHeader
+}
+</script>
+
+<style>
+
+</style>
